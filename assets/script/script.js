37956f24ca27d83778e4783bc5faf6c295358767
Abhinav <singh.abhinav6996@gmail.com>
2019-08-03 13:43:05 +0530

    the-dad-joke

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,54 @@
 const query = `
-qyuery{
-    joke{
-        joke
-        permalink
+  query { 
+    joke {
+      joke
+      permalink
     }
-}
+  }
 `;
-//fetch joke from api and dispaly on screen
+//fetch api and display random joke
 const randomJoke = () => {
   fetch(`https://icanhazdadjoke.com/graphql?query=${query}`)
     .then(response => response.json())
     .then(data => {
       const { joke } = data.data.joke;
-      window.joke.innerHTML = `<div>${joke}</div>`;
+      window.joke.innerHTML = `
+      <div>${joke}</div>
+    `;
     });
 };
-//call joke function
+//joke call
 randomJoke();
-//button variable
+//click button variable
 const jokeBtn = document.querySelector(".joke-js");
-//screen variable
+
 const screen = document.querySelector(".screen");
-//button event listener
 jokeBtn.addEventListener("click", () => {
-  let colorLight2;
+  randomJoke();
+  changeColor(getColor());
 });
+
+const changeColor = color => {
+  screen.style.background = color;
+};
+
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const rand = (min, max) => {
+  return parseInt(Math.random() * (max - min + 1), 10) + min;
+};
+const getColor = () => {
+  let h = rand(1, 360); // color hue between 1 and 360
+  let s = rand(30, 100); // saturation 30-100%
+  let l = rand(30, 100); // lightness 30-70%
+  localStorage.setItem("light", l);
+  let colorHsl = "hsl(" + h + "," + s + "%," + l + "%)";
+  return colorHsl;
+};
